1c0e0e7f30be859ef1e78a3d4edd8831791944dd
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-07-29 23:13:30 +0800

    feat: UrlInfo Data type and stub

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO url_info (id, url, title, description, thumbnail) VALUES
+(1, 'http://example.com', 'Example Domain', 'This domain is for use in illustrative examples in documents.', 'http://example.com/image.jpg'),
+(2, 'http://example.org', 'Another Example', 'Another example description.', 'http://example.org/image.jpg');
