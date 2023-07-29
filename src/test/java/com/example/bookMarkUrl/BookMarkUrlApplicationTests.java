128f2b5918c58e4663c5a4f54bbd464f6f35fd84
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-07-29 22:28:55 +0800

    feat: hello world

---
+++ src/test/java/com/example/bookMarkUrl/BookMarkUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookMarkUrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarkUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
