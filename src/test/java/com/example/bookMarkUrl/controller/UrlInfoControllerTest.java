0eb7631d883e9de217be0380077009ded6ca2c25
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-08-20 21:54:19 +0800

    test: temporary only test case title

--- src/test/java/com/example/bookMarkUrl/controller/UrlInfoControllerTest.java
+++ src/test/java/com/example/bookMarkUrl/controller/UrlInfoControllerTest.java
@@ -65,39 +65,9 @@ class UrlInfoControllerTest {
     Mockito.verify(urlInfoService, Mockito.times(1)).scrapeAndSaveUrl(Mockito.anyString());
   }
 
-  // 本当はフロントで'設定なし'の文言が表示されることをテストしたいがReact化するからそっちでテストする
   @Test
-  @DisplayName("追加するURLにディスクリプションとサムネイル画像が設定されていない場合でも追加できる")
-  public void shouldDisplayTextNoSetting() throws Exception {
-    // HTMLドキュメントオブジェクトの作成
-    Document document = new Document("");
-    // タイトルの要素の追加
-    Element titleElement = new Element("meta");
-    titleElement.attr("property", "og:title");
-    titleElement.attr("content", "Example Title");
-    document.head().appendChild(titleElement);
-
-    // 説明の要素の追加
-    Element descriptionElement = new Element("meta");
-    descriptionElement.attr("property", "og:description");
-    descriptionElement.attr("content", "");
-    document.head().appendChild(descriptionElement);
-
-    // サムネイルの要素の追加
-    Element thumbnailElement = new Element("meta");
-    thumbnailElement.attr("property", "og:image");
-    thumbnailElement.attr("content", "");
-    document.head().appendChild(thumbnailElement);
-
-    // urlScraperServiceが呼ばれた時にDocumentオブジェクトを返す
-    Mockito.doNothing().when(urlInfoService).scrapeAndSaveUrl(Mockito.anyString());
-
-    mockMvc.perform(MockMvcRequestBuilders.post("/add").param("url", "https://example.com"))
-      .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-      .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
-
-    Mockito.verify(urlInfoService, Mockito.times(1)).scrapeAndSaveUrl(Mockito.anyString());
-  }
+  @DisplayName("追加したURLにディスクリプションとサムネイル画像が設定されていない場合は'設定なし'の文言が表示される")
+  public void shouldDisplayTextNoSetting() throws Exception {}
 
   @Test
   @DisplayName("登録されているURLを削除することができる")
@@ -110,4 +80,16 @@ class UrlInfoControllerTest {
 
     verify(urlInfoRepository, times(1)).deleteById(idToDelete);
   }
+
+  @Test
+  @DisplayName("emailとpasswordの登録でユーザーを登録することができる")
+  public void shouldUserRegister() throws Exception {}
+
+  @Test
+  @DisplayName("登録されているユーザーでログインすることができる")
+  public void shouldUserLogin() throws Exception {}
+
+  @Test
+  @DisplayName("追加したURLをお気に入りとして登録できる")
+  public void shouldFavoriteRegisterUrl() throws Exception {}
 }
