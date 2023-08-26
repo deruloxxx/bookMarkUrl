c2c562dd110081baa9aedf8b7149018101de4b33
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-08-26 15:19:38 +0800

    feat: login by register user

--- src/main/java/com/example/bookMarkUrl/service/MUserService.java
+++ src/main/java/com/example/bookMarkUrl/service/MUserService.java
@@ -4,15 +4,20 @@ import com.example.bookMarkUrl.entity.MUser;
 import com.example.bookMarkUrl.repository.MUserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MUserService {
   @Autowired
+  private PasswordEncoder passwordEncoder;
+  @Autowired
   MUserRepository userRepository;
 
   @Transactional
   public MUser createUser(MUser user) {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    user.setRole("ROLE_USER");
     return userRepository.save(user);
   }
 }
